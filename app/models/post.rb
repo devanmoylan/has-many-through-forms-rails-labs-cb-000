d5714d5cc65b45dd77fca01228bb4a7b41857aee
Devan <devan.moylan@backyardlion.com>
2017-07-24 15:49:57 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
   has_many :comments
   has_many :users, through: :comments
 
